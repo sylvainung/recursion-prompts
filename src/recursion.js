5dad912a349429098ddd94c8cd3880c6865fe5bb
Sylvain Ung <sylvainung@live.com>
2017-11-23 19:40:28 -0800

    Solved exponent

--- src/recursion.js
+++ src/recursion.js
@@ -102,8 +102,9 @@ var range = function(x, y) {
     return result;
   } 
 
-  if ((x < y && nextNumber < y) || (x > y && nextNumber > y))
-  result.push(nextNumber);
+  if ((x < y && nextNumber < y) || (x > y && nextNumber > y)) {
+    result.push(nextNumber);
+  }
 
   return result.concat(range(nextNumber, y))
 };
@@ -114,6 +115,21 @@ var range = function(x, y) {
 // exponent(4,3); // 64
 // https://www.khanacademy.org/computing/computer-science/algorithms/recursive-algorithms/a/computing-powers-of-a-number
 var exponent = function(base, exp) {
+  if (exp === 0) {
+    return 1;
+  }
+
+  if (exp === 1) {
+    return base;
+  }
+  
+  if (exp > 0) {
+    return base * exponent(base, exp - 1);
+  }
+
+  if (exp < 0) {
+    return (1 / exponent(base, exp * -1));
+  }
 };
 
 // 8. Determine if a number is a power of two.
