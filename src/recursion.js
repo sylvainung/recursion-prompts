11aee249e7a2d7333589cdd6336114cced77d527
Sylvain Ung <sylvainung@live.com>
2017-11-24 16:10:03 -0800

    Solved reverseArr

--- src/recursion.js
+++ src/recursion.js
@@ -237,6 +237,15 @@ var createArray = function(str) {
 
 // 17. Reverse the order of an array
 var reverseArr = function(array) {
+  var result = [];
+
+  if (!array.length) {
+    return result;
+  }
+
+  result.push(array.pop());
+
+  return result.concat(reverseArr(array));
 };
 
 // 18. Create a new array with a given value and length.
